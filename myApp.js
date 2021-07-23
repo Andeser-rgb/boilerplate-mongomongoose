fd9a87d92aa93a99fcbd42b0d1306ba3f17bcf43
Andeser-rgb <andeser@protonmail.com>
2021-07-23 11:37:39 +0200

    Added function findAndUpdate

--- myApp.js
+++ myApp.js
@@ -63,8 +63,8 @@ const findEditThenSave = (personId, done) => {
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
+  Person.findOneAndUpdate({name: personName}, {age: ageToSet}, {new: true});
 
-  done(null /*, data*/);
 };
 
 const removeById = (personId, done) => {
